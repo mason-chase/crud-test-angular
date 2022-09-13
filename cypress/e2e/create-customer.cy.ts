a7724327d5ad165a83df99c68015ab81e188a832
aliShoghian <shoghianpoorali@gmail.com>
2022-09-13 11:46:32 +0430

    wrote customer form e2e test

---
+++ cypress/e2e/create-customer.cy.ts
@@ -0,0 +1,46 @@
+
+const Firstname='[formControlName="Firstname"]';
+const Lastname='[formControlName="Lastname"]';
+const DateOfBirth='[formControlName="DateOfBirth"]';
+const PhoneNumber='[formControlName="PhoneNumber"]';
+const Email='[formControlName="Email"]';
+const BankAccountNumber='[formControlName="BankAccountNumber"]';
+const submit =  '[data-cy="submit-customer-form"]';
+describe('visit the create customer ', () => {
+
+    it('visit the create customer page', () => {
+        cy.visit('/customer/create');
+    })
+    it('check all of form controls are exist', () => {
+        cy.get(Firstname).should('exist')
+        cy.get(Lastname).should('exist')
+        cy.get(DateOfBirth).should('exist')
+        cy.get(PhoneNumber).should('exist')
+        cy.get(Email).should('exist')
+        cy.get(BankAccountNumber).should('exist')
+    })
+    it('Invalid phone number formate message is exist',()=>{
+        cy.get(PhoneNumber).type('sdf23432424');
+        cy.get(Email).focus().then(()=>{
+            cy.get('[data-cy="invalid-phone-number"]').should('exist')
+        });
+    })
+    it('Invalid email address formate message is exist',()=>{
+        cy.get(Email).type('sdf23432424@');
+        cy.get(PhoneNumber).focus().then(()=>{
+            cy.get('[data-cy="invalid-email"]').should('exist');
+
+        });
+    })
+    it('Enabled save button after form is valid',()=>{
+        cy.get(PhoneNumber).clear();
+        cy.get(Email).clear();
+        cy.get(Firstname).type('Ali');
+        cy.get(Lastname).type('shoghian');
+        cy.get(DateOfBirth).type('1992-12-30');
+        cy.get(PhoneNumber).type('2025550195');
+        cy.get(Email).type('shoghianpoorali@gmail.com')
+        cy.get(BankAccountNumber).type('322');
+        cy.get(submit).should('not.be.disabled')
+    })
+})
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,18 +1,6 @@
-describe('My First Test', () => {
+describe('test initial app', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
     cy.location('pathname').should('eq','/customer/create')
-  })
-  it('visit the create customer page',()=>{
-    cy.visit('/customer/create');
-  })
-  it('check all of form controls are exist',()=>{
-    
-    cy.get('[formControlName="Firstname"]').should('exist')
-    cy.get('[formControlName="Lastname"]').should('exist')
-    cy.get('[formControlName="DateOfBirth"]').should('exist')
-    cy.get('[formControlName="PhoneNumber"]').should('exist')
-    cy.get('[formControlName="Email"]').should('exist')
-    cy.get('[formControlName="BankAccountNumber"]').should('exist')
   })
 })
