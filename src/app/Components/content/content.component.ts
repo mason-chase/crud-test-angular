a74a9c3317a50429d6107cd426d7f0ea5896cc32
davodbeheshti <davood.beheshti2018@gmail.copm>
2023-01-11 14:57:12 -0800

    fixes function submit

--- src/app/Components/content/content.component.ts
+++ src/app/Components/content/content.component.ts
@@ -20,7 +20,44 @@ export class ContentComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       bankAccountNumber: new FormControl('', Validators.required),
     })
+  submit() {
+    if (this.form.valid) {
+      const { firstName, lastName, dateOfBirth, email } = this.form.value;
+      const date = moment(dateOfBirth).format('dd/mm/yy');
+      if (this.customers.length === 0) {
+        this.form.get('dateOfBirth').setValue(date);
+        this.customers.push(this.form.value);
+        localStorage.setItem('customers', JSON.stringify(this.customers));
+        this.form.reset();
+        this.messageValidator(null);
+      } else {
+        this.messageValidator(null);
+        this.customers.map(customer => {
+          if (customer.firstName.includes(firstName)) {
+            this.messageValidator('firstName')
+          }
+          if (customer.lastName.includes(lastName)) {
+            this.messageValidator('lastName')
+          }
+          if (customer.dateOfBirth.includes(date)) {
+            this.messageValidator('dateOfBirth')
+          }
+          if (customer.email.includes(email)) {
+            this.messageValidator('email')
+          }
+          if (this.validDataCustomer) {
+            this.form.get('dateOfBirth').setValue(date);
+            this.customers.push(this.form.value);
+            localStorage.setItem('customers', JSON.stringify(this.customers));
+            this.form.reset();
+            this.messageService.add({ severity: 'success', detail: 'SuccessFully' });
+            this.messageValidator(null)
+          }
+        })
+      }
+    } else {
       this.messageService.add({ severity: 'error', summary: 'error', detail: 'is not valid form' });
+    }
   }
 
 }
