ccf17f5e735f7c5c0abf3cc08f833cacbb44c489
davodbeheshti <davood.beheshti2018@gmail.copm>
2023-01-11 11:29:37 -0800

    add modules form in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './Shared/shared.module';
@@ -15,6 +16,9 @@ import { FooterComponent } from './Components/footer/footer.component';
   ],
   imports: [
     BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
     SharedModule,
   ],
   providers: [],
