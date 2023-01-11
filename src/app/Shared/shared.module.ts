bb028906206775114226d33c848483787e8587d4
davodbeheshti <davood.beheshti2018@gmail.copm>
2023-01-11 11:31:11 -0800

    add module inputText and calendar in shared

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
+// import primeng packages
+import { InputTextModule } from 'primeng/inputtext';
+import {CalendarModule} from 'primeng/calendar';
+
+const Import_Export_PackagePrimeng = [InputTextModule , CalendarModule]
+
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    Import_Export_PackagePrimeng
+  ],
+  exports: [Import_Export_PackagePrimeng]
 })
 export class SharedModule { }
