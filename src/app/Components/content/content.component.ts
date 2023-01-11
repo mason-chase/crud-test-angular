b084e1e4ecca6639ccca5157ee7d10aee696f08d
davodbeheshti <davood.beheshti2018@gmail.copm>
2023-01-11 14:53:44 -0800

    apply form with reactiveForm

--- src/app/Components/content/content.component.ts
+++ src/app/Components/content/content.component.ts
@@ -10,6 +10,14 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      dateOfBirth: new FormControl('', Validators.required),
+      phoneNumber: new FormControl('', PhoneNumberValidator('US')),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      bankAccountNumber: new FormControl('', Validators.required),
+    })
   }
 
 }
