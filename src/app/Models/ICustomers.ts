2f37ccc1a55b928eb76972569c3c21634320f13c
davodbeheshti <davood.beheshti2018@gmail.copm>
2023-01-11 14:56:43 -0800

    add model customer

---
+++ src/app/Models/ICustomers.ts
@@ -0,0 +1,8 @@
+export interface ICustomer {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    phoneNumber: string;
+    email: string,
+    bankAccountNumber: number;
+}
