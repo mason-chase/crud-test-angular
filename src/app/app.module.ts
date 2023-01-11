4362c9f3420bba7b28c400bf8a383ae1ad7669af
davodbeheshti <davood.beheshti2018@gmail.copm>
2023-01-11 11:29:01 -0800

    add module share in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './Shared/shared.module';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 
@@ -14,6 +15,7 @@ import { FooterComponent } from './Components/footer/footer.component';
   ],
   imports: [
     BrowserModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
