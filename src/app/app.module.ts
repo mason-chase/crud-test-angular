2bc1eafbf3ffc46d11cf76217233c4e3e04ed0bc
IamGrootie <ligiatmonteiro@gmail.com>
2023-03-05 19:42:58 +0000

    reset css and added global styles. Created main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ClientsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
