8f94f0f3632f58c044de74f5cd4f6709511d24fb
IamGrootie <ligiatmonteiro@gmail.com>
2023-03-08 18:04:40 +0000

    Fixed placeholders and thead text orientation.

--- src/app/initClients.ts
+++ src/app/initClients.ts
@@ -1,17 +1,7 @@
 export class Init {
     load() {
       if(localStorage.getItem('clients') === null || localStorage.getItem('clients') == undefined) {
-        let clients = [
-          {
-            firstName: 'John',
-            lastName: 'Doe',
-            dateOfBirth: '1992-04-12',
-            phoneNumber: '+351968765728',
-            email: 'test@example.com',
-            bankAccountNumber: '123457890',
-          }
-        ];
-  
+        let clients: never[] = [];
         localStorage.setItem('clients', JSON.stringify(clients));
         return;
       } 
