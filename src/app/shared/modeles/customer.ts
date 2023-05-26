79a33edb72b647451b75d8b107785dcdafb581ad
shayan ghazali <shayan97.tti@gmail.com>
2023-05-26 21:13:55 +0430

    add phone validator and customer model

---
+++ src/app/shared/modeles/customer.ts
@@ -0,0 +1,9 @@
+export interface Customer{
+      id:number;
+      firstname:string;
+      lastname:string;
+      dateOfBirth:string;
+      phoneNumber:string;
+      email:string;
+      bankAccountNumber:string;
+  }
---
+++ src/app/shared/validators/custom-validators.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidatorFn } from "@angular/forms";
+import { PhoneNumberUtil } from "google-libphonenumber";
+
+const phoneNumberUtil = PhoneNumberUtil.getInstance();
+
+export function PhoneNumberValidator(regionCode: string): ValidatorFn {
+  return (control: AbstractControl) => {
+    let validNumber = false;
+    try {
+      const phoneNumber = phoneNumberUtil.parseAndKeepRawInput(
+        control.value, regionCode
+      );
+      validNumber = phoneNumberUtil.isValidNumber(phoneNumber);
+    } catch (e) { }
+    return validNumber ? null : { 'wrongNumber': { value: control.value } };
+  }
+}
