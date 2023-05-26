21695e6739da6d7c5b1a46e28db54f0dfcd39eee
shayan ghazali <shayan97.tti@gmail.com>
2023-05-26 21:15:15 +0430

    package and global styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CrudCustomerComponent } from './crud-customer/crud-customer.component';
+import { CustomerDialogComponent } from './customer-dialog/customer-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudCustomerComponent,
+    CustomerDialogComponent,
+    DescriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
