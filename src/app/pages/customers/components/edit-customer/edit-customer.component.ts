ba11a69ba9b7cc8f100de5855f0859ea4ad21f56
sadegh salari nia <mrsadeghsn@gmail.com>
2023-06-02 16:22:41 +0330

    Feature: edit customer complete without validation

--- src/app/pages/customers/components/edit-customer/edit-customer.component.ts
+++ src/app/pages/customers/components/edit-customer/edit-customer.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ICustomer } from '../../models/customer.interface';
 
 @Component({
   selector: 'app-edit-customer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './edit-customer.component.html',
   styleUrls: ['./edit-customer.component.css']
 })
 export class EditCustomerComponent {
+  edit_form!: FormGroup;
+  edit_mood: boolean = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private _dialogRef: MatDialogRef<EditCustomerComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { customer: ICustomer, edit_mood: boolean },
+
+  ) {
+    this.edit_mood = data.edit_mood;
+    this.edit_form = this.formBuilder.group({
+      Firstname: new FormControl({ value: data.customer.Firstname, disabled: false }),
+      Lastname: new FormControl({ value: data.customer.Lastname, disabled: false }),
+      DateOfBirth: new FormControl({ value: data.customer.DateOfBirth, disabled: false }),
+      PhoneNumber: new FormControl({ value: data.customer.PhoneNumber, disabled: false }),
+      Email: new FormControl({ value: data.customer.Email, disabled: false }),
+      BankAccountNumber: new FormControl({ value: data.customer.BankAccountNumber, disabled: false }),
+    })
+  }
+
+  editHandler() {
+    this._dialogRef.close(this.edit_form.value)
+  }
 
 }
--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -1,51 +1,18 @@
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ICustomer } from './models/customer.interface';
 import { LocalStorage } from 'src/shared/utils/local-storage';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
 import { CustomerItemComponent } from './components/customer-item/customer-item.component';
-
-const FakeData: ICustomer[] = [
-  {
-    Firstname: 'sadegh',
-    Lastname: 'sallari nia',
-    Email: 'mrsadeghsn@gmail.com',
-    PhoneNumber: '+989302940504',
-    DateOfBirth: '21/1/1996',
-    BankAccountNumber: '123'
-  },
-  {
-    Firstname: 'sadegh',
-    Lastname: 'sallari nia',
-    Email: 'mrsadeghsn@gmail.com',
-    PhoneNumber: '+989302940504',
-    DateOfBirth: '21/1/1996',
-    BankAccountNumber: '123'
-  },
-  {
-    Firstname: 'sadegh',
-    Lastname: 'sallari nia',
-    Email: 'mrsadeghsn@gmail.com',
-    PhoneNumber: '+989302940504',
-    DateOfBirth: '21/1/1996',
-    BankAccountNumber: '123'
-  },
-  {
-    Firstname: 'sadegh',
-    Lastname: 'sallari nia',
-    Email: 'mrsadeghsn@gmail.com',
-    PhoneNumber: '+989302940504',
-    DateOfBirth: '21/1/1996',
-    BankAccountNumber: '123'
-  },
-]
-
+import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
   imports: [
     CommonModule,
+    MatDialogModule,
     CustomerItemComponent,
   ],
   templateUrl: './customers.component.html',
@@ -56,11 +23,11 @@ export class CustomersComponent implements OnInit {
   customers$: Observable<ICustomer[]> = of()
 
   constructor(
+    private _dialog: MatDialog,
     private localStorage: LocalStorage,
   ) { }
 
   ngOnInit(): void {
-    this.localStorage.setItem('CUSTOMERS', JSON.stringify(FakeData));
     this.customers$ = this.getCustomers();
   }
 
@@ -71,11 +38,53 @@ export class CustomersComponent implements OnInit {
     return of(customers)
   }
 
-  edit(customer: ICustomer) {
+  create() {
+    let dialog = this._dialog.open(EditCustomerComponent, {
+      panelClass: 'edit-dialog',
+      data: {
+        customer: {},
+        edit_mood: false
+      }
+    });
+  }
 
+  edit(customer: ICustomer) {
+    let dialog = this._dialog.open(EditCustomerComponent, {
+      panelClass: 'edit-dialog',
+      data: {
+        customer,
+        edit_mood: true
+      }
+    });
+
+    dialog.afterClosed().subscribe((res: ICustomer) => {
+      this.customers$ = this.customers$.pipe(
+        map(customers => {
+          let index = customers.findIndex(x => x.PhoneNumber === res.PhoneNumber);
+          if (index > -1) {
+            customers[index] = res;
+          }
+          this.handleChanges(customers);
+          return customers;
+        })
+      );
+    });
   }
 
   remove(customer: ICustomer) {
+    this.customers$ = this.customers$.pipe(
+      map(customers => {
+        let index = customers.findIndex(x => x.Email === customer.Email);
+        if (index > -1) {
+          customers.splice(index, 1)
+        }
+        this.handleChanges(customers);
+        return customers;
+      })
+    );
+  }
 
+  handleChanges(customers: ICustomer[]) {
+    this.localStorage.setItem('CUSTOMERS', JSON.stringify(customers));
   }
 }
