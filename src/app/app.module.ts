e22219b2ccc4ef9de613c13a7e431022c4c04563
hossein.ganjyar@gmail.com <hossein.ganjyar@gmail.com>
2023-08-03 22:20:05 +0330

    (initial) feat: create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
