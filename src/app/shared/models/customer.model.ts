b12d05564722caff442b5439864dd42b7263ac77
mojtaba <cems92@yahoo.com>
2023-12-12 01:40:01 +0330

    customer model created

---
+++ src/app/shared/models/customer.model.ts
@@ -0,0 +1,9 @@
+export interface CustomerModel {
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: string;
+  phoneNumber?: string;
+  email?: string;
+  bankAccountNumber?: string;
+  id?: number;
+}
